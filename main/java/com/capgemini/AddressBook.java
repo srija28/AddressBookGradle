2f781b6d452c99c80f43f183b761590265505b7b
shreya <shreyareddykethireddy@shreyas-MacBook-Air.local>
2020-10-03 02:25:59 +0530

    [Shreya]Added:details from user

--- main/java/com/capgemini/AddressBook.java
+++ main/java/com/capgemini/AddressBook.java
@@ -1,10 +1,54 @@
 package com.capgemini;
 
+import java.util.*;
+
 public class AddressBook {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Addressbook");	
-		Contacts contact = new Contacts();
-		
-	} 
 
+		Scanner sc = new Scanner(System.in);
+		boolean matchFound = false;
+		System.out.println("Welcome to Addressbook");
+
+		Contacts person = new Contacts();
+		System.out.print("First Name: ");
+		while (!matchFound) {
+			matchFound = person.setFirstName(sc.next());
+		}
+		matchFound = false;
+		System.out.print("Last Name: ");
+		while (!matchFound) {
+			matchFound = person.setLastName(sc.next());
+		}
+		matchFound = false;
+		System.out.print("Address: ");
+		while (!matchFound) {
+			matchFound = person.setAddress(sc.next());
+		}
+		matchFound = false;
+		System.out.print("City: ");
+		while (!matchFound) {
+			matchFound = person.setCity(sc.next());
+		}
+		matchFound = false;
+		System.out.print("State: ");
+		while (!matchFound) {
+			matchFound = person.setState(sc.next());
+		}
+		matchFound = false;
+		System.out.print("Zip: ");
+		while (!matchFound) {
+			matchFound = person.setZip(sc.next());
+		}
+		matchFound = false;
+		System.out.print("Phone Number: ");
+		while (!matchFound) {
+			matchFound = person.setPhoneNo(sc.next());
+		}
+		matchFound = false;
+		System.out.print("Email: ");
+		while (!matchFound) {
+			matchFound = person.setEmail(sc.next());
+		}
+		System.out.println("Thank You");
+	}
 }
